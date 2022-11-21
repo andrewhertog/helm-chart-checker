4a6e57ce55a2bef5fbd4491be8edc6bba782a218
Andrew den Hertog <andrew@denhertog.ca>
2022-11-21 15:51:46 -0500

    cleanup and highlight out of date charts

--- main.go
+++ main.go
@@ -6,26 +6,4 @@ import (
 
 func main() {
 	cmd.Execute()
-	// charts := []helm.Chart{}
-	// charts := helm.Charts{}
-	// resource := kubernetes.Resource{}
-	// resource.Resource("helm.fluxcd.io", "v1", "helmreleases")
-	// namespace := "system-releases"
-	// resource.GetHelmReleaseForNamespace(namespace)
-
-	// for _, item := range resource.Content {
-	// 	chart := helm.GetChartSpec(item.Object)
-	// 	if !funk.Contains(charts.Charts, *chart) {
-	// 		charts.Append(chart)
-	// 	}
-	// }
-	// for i, chart := range charts.Charts {
-	// 	if chart.IsHelmRepo() {
-	// 		chart.GetLatestChartVersion()
-	// 		charts.Charts[i] = chart
-	// 	}
-
-	// }
-	// charts.PrintChartData()
-
 }
